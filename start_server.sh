90e7fcec0c2621a3507377c781dfc33e198440ae
sjyan <i@yanshengjia.com>
2018-05-15 15:11:29 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+echo "Cleaning..."
 mvn clean
+
+echo "Building..."
 ./build.sh languagetool-standalone package -DskipTests
+
+echo "Translating..."
+rm ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT/org/languagetool/rules/en/grammar.xml
+cp ./rules/grammar_zh.xml ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT/org/languagetool/rules/en/grammar.xml
+
+echo "Start the server"
 cd ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT
 nohup java -cp languagetool-server.jar org.languagetool.server.HTTPServer &
