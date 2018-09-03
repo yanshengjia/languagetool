8dbe0966f762fbf3c822a592efd65a49d55ad7d6
sjyan <i@yanshengjia.com>
2018-09-03 12:37:08 +0800

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -11,7 +11,7 @@ echo "Building..."
 
 echo "Translating..."
 rm ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT/org/languagetool/rules/en/grammar.xml
-cp ./rules/grammar_zh.xml ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT/org/languagetool/rules/en/grammar.xml
+cp ./rules/grammar.xml ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT/org/languagetool/rules/en/grammar.xml
 
 echo "Start the server"
 cd ./languagetool-standalone/target/LanguageTool-4.2-SNAPSHOT/LanguageTool-4.2-SNAPSHOT
